4fc69d743e31d633526097a4f28148329f8822a6
Sarmed Rizvi <sarmed@Sarmeds-MacBook-Pro.local>
2024-11-21 00:28:15 +0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,139 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useCallback, useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  FlatList,
+  ListRenderItem,
+  Button,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
 } from 'react-native';
-
+import {useEffect} from 'react';
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  AudioSession,
+  LiveKitRoom,
+  useTracks,
+  TrackReferenceOrPlaceholder,
+  VideoTrack,
+  isTrackReference,
+} from '@livekit/react-native';
+import {Track} from 'livekit-client';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const wsURL = 'wss://flutterflow-uwvr67ix.livekit.cloud';
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzIxMjc1NTcsImlzcyI6IkFQSW80SHc2TG50b2VrUCIsIm5iZiI6MTczMjEyNjY1Nywic3ViIjoic2FtIiwidmlkZW8iOnsicm9vbSI6InNhcm1lZCIsInJvb21Kb2luIjp0cnVlLCJyb29tTGlzdCI6dHJ1ZX19.PERxvMotkGinZMUbd2kk6_j8gs3CvHN-JEVizgtuaKs';
+function onDeviceFailure(error: any) {
+  console.error(error, 'onDeviceFailure');
+}
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [connectionDetails, updateConnectionDetails] = useState(undefined);
+  const [agentState, setAgentState] = useState('disconnected');
+
+  const onConnectButtonClicked = useCallback(async () => {
+    // Generate room connection details, including:
+    //   - A random Room name
+    //   - A random Participant name
+    //   - An Access Token to permit the participant to join the room
+    //   - The URL of the LiveKit server to connect to
+    //
+    // In real-world application, you would likely allow the user to specify their
+    // own participant name, and possibly to choose from existing rooms to join.
+
+    // const url = new URL('http://localhost:3000/api/connection-details');
+    try {
+      const response = await fetch(
+        'http://10.0.2.2:3000/api/connection-details',
+        {
+          method: 'GET',
+        },
+      );
+      console.log({response});
+      const connectionDetailsData = await response.json();
+      console.log({connectionDetailsData});
+      updateConnectionDetails(connectionDetailsData);
+    } catch (error) {
+      console.log({error});
+    }
+  }, []);
+  console.log({connectionDetails});
+  // Start the audio session first.
+  useEffect(() => {
+    let start = async () => {
+      await AudioSession.startAudioSession();
+    };
+
+    start();
+    return () => {
+      AudioSession.stopAudioSession();
+    };
+  }, []);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <LiveKitRoom
+      token={connectionDetails?.participantToken}
+      serverUrl={connectionDetails?.serverUrl}
+      connect={connectionDetails !== undefined}
+      audio={true}
+      video={false}
+      onMediaDeviceFailure={onDeviceFailure}
+      onDisconnected={() => {
+        updateConnectionDetails(undefined);
+      }}
+      options={{
+        // Use screen pixel density to handle screens with differing densities.
+        adaptiveStream: {pixelDensity: 'screen'},
+      }}>
+      <RoomView onConnectButtonClicked={onConnectButtonClicked} />
+      {/* <VoiceAssistantControlBar controls={{ leave: false }} /> */}
+    </LiveKitRoom>
+  );
+}
+
+const RoomView = ({onConnectButtonClicked}: any) => {
+  // Get all camera tracks.
+  // The useTracks hook grabs the tracks from LiveKitRoom component
+  // providing the context for the Room object.
+  // const tracks = useTracks([Track.Source.Camera]);
+
+  // const renderTrack: ListRenderItem<TrackReferenceOrPlaceholder> = ({item}) => {
+  //   console.log({item});
+  //   // Render using the VideoTrack component.
+  //   if (isTrackReference(item)) {
+  //     return <VideoTrack trackRef={item} style={styles.participantView} />;
+  //   } else {
+  //     return <View style={styles.participantView} />;
+  //   }
+  // };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.participantView}>
+        <TouchableHighlight
+          onPress={onConnectButtonClicked}
+          style={{
+            borderRadius: 23,
+          }}>
+          <Text>Start Conversation</Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  participantView: {
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'black',
+    // borderColor: 'yellow',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  participantViewButton: {},
 });
-
-export default App;
